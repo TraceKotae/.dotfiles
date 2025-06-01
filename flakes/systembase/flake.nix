b10def7e26c9fe0a0442a10350c1d0fb78f74d4c
daniel <dansmediacorner@gmail.com>
2025-06-01 21:01:36 +0200

    remove mount

--- flakes/systembase/flake.nix
+++ flakes/systembase/flake.nix
@@ -22,17 +22,17 @@ services.pipewire = {
 services.gnome.gnome-keyring.enable = true;
   
 # Mount Harddrives
-  fileSystems."/mnt/4TBintSSD" = {
-    device = "/dev/disk/by-uuid/ebc9f941-239d-4bfa-8e53-e7e18f786350"; # REPLACE with your actual UUID
-    fsType = "btrfs"; # REPLACE with your filesystem type (e.g., "btrfs", "xfs")
-    options = [
-          "defaults"
-          "nofail"
-          "uid=1000" # Replace with your user's UID
-          "gid=100"  # Replace with your user's GID (usually "users" group)
-          # "compress-force=zstd" # Add other options as needed, e.g., "compress-force=zstd" for btrfs
-        ];
- };
+#  fileSystems."/mnt/4TBintSSD" = {
+#    device = "/dev/disk/by-uuid/ebc9f941-239d-4bfa-8e53-e7e18f786350"; # REPLACE with your actual UUID
+#    fsType = "btrfs"; # REPLACE with your filesystem type (e.g., "btrfs", "xfs")
+#    options = [
+#          "defaults"
+#          "nofail"
+#          "uid=1000" # Replace with your user's UID
+#          "gid=100"  # Replace with your user's GID (usually "users" group)
+#          # "compress-force=zstd" # Add other options as needed, e.g., "compress-force=zstd" for btrfs
+#        ];
+# };
 };
 };
 }
