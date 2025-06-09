c9ab8d1ec1bfc8d4be15459a32687562d34656d8
TraceKotae <55962569+TraceKotae@users.noreply.github.com>
2025-06-09 23:42:33 +0200

    Update boot.nix

--- flakes/desktopbase/boot.nix
+++ flakes/desktopbase/boot.nix
@@ -2,12 +2,8 @@
 
 {
   boot = {
-    kernelPackages = pkgs.linuxPackages_zen;
     kernelModules = [ "v4l2loopback" ];
     extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-    kernel.sysctl = { "vm.max_map_count" = 2147483642; };
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = true;
     # Appimage Support
     binfmt.registrations.appimage = {
       wrapInterpreterInShell = false;
