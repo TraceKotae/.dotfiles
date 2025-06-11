a78de2f412cb8b34fc04fa7f377270ed5a57bb9e
trace <dansmediacorner@gmail.com>
2025-06-11 23:19:23 +0200

    Remove Duplicate Settings
    Add steam settings

--- flakes/usersoftware/flake.nix
+++ flakes/usersoftware/flake.nix
@@ -18,10 +18,18 @@ outputs = { self, nixpkgs, ... }: {
    gparted
    ];
   programs = {
-  steam.enable = true;
+  
+  steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+  
   gamemode.enable = true;
   coolercontrol.enable = true;
   coolercontrol.nvidiaSupport = true;
+ 
   };
   };
  };
