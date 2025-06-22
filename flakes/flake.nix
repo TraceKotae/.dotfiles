52deccca68d2142655300fe264321c4fca65ac01
TraceKotae <55962569+TraceKotae@users.noreply.github.com>
2025-06-22 20:46:29 +0200

    Update flake.nix

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -3,7 +3,7 @@ description = "Toplevel Flake";
 
 inputs = {
 nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-#stylix.url = "github:nix-community/stylix/release-25.05";
+stylix.url = "github:nix-community/stylix/release-25.05";
 desktopbase.url = "path:./desktopbase";
 systembase.url = "path:./systembase";
 nvidia.url = "path:./nvidia";
@@ -12,13 +12,13 @@ home-manager.inputs.nixpkgs.follows = "nixpkgs";
 usersoftware.url = "path:./usersoftware";
 };
 
-outputs = { self, nixpkgs, desktopbase, systembase, home-manager, nvidia, usersoftware, ... } @ inputs: {
+outputs = { self, nixpkgs, desktopbase, systembase, home-manager, nvidia, usersoftware, stylix, ... } @ inputs: {
  nixosConfigurations = {
   "nixos" = nixpkgs.lib.nixosSystem {
    system = "x86_64-linux";
    modules = [
     ./configuration.nix
-#    stylix.nixosModules.stylix
+    stylix.nixosModules.stylix
     desktopbase.nixosModules.default
     systembase.nixosModules.default
     nvidia.nixosModules.default
