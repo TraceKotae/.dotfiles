b1c49bc3e2b1cf821cac287b9961351c5e902601
TraceKotae <55962569+TraceKotae@users.noreply.github.com>
2025-06-22 19:28:56 +0200

    Update configuration.nix

--- flakes/configuration.nix
+++ flakes/configuration.nix
@@ -13,7 +13,7 @@
   boot.loader.systemd-boot.enable = false;
 #  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dec/nvme1n1p1";
+  boot.loader.grub.device = "/dec/nvme1n1";
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.efiSupport = true;
 
