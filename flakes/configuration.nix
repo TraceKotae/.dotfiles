0efc3cf1dbdb84fbba40a8eeaaf0012c18768d39
Daniel Moron <dansmediacorner@gmail.com>
2025-06-22 18:46:01 +0200

    Switch to Grup with UEFI support

--- flakes/configuration.nix
+++ flakes/configuration.nix
@@ -10,11 +10,12 @@
       /etc/nixos/hardware-configuration.nix
     ];
   #Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  #boot.loader.grub.enable = true;
-  #boot.loader.grub.device = "/dev/sda";
-  #boot.loader.grub.useOSProber = true;
+#  boot.loader.systemd-boot.enable = true;
+#  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
