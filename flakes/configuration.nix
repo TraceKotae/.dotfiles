0e7fded596681286b19683f6cc631d7e469aee17
Trace <tracekotae@gmail.com>
2025-06-28 00:22:55 +0200

    add stylix for dependencies

--- flakes/configuration.nix
+++ flakes/configuration.nix
@@ -30,5 +30,34 @@
    git
    ntfs3g #to read ntfs drives this should also make windows efi show up in grub as bootable options.
    ];
-
+   
+#THEMING   
+stylix.enable = true;
+stylix.targets.gtk.enable = true;
+stylix.targets.qt.enable = true;
+stylix.autoEnable = true;
+stylix.cursor.package = pkgs.bibata-cursors;
+stylix.cursor.name = "Bibata-Modern-Ice";
+stylix.cursor.size = 24;
+stylix.polarity = "dark";
+stylix.image = ./Assets/Wallpapers/SakuratreeLarge.png;
+stylix.opacity.terminal = 0.5;
+stylix.opacity.applications = 0.5;
+stylix.opacity.desktop = 0.5;
+stylix.opacity.popups = 0.5;
+stylix.fonts = {
+	monospace = {
+		package = pkgs.nerd-fonts.jetbrains-mono;
+		name = "JetBrainsMono Nerd Font Mono";
+		};
+		
+	sansSerif = {
+		package = pkgs.dejavu_fonts;
+		name = "DejaVu Sans";
+		};
+	serif = {
+		package = pkgs.dejavu_fonts;
+		name = "DejaVu Serif";
+		};
+	};
 }
