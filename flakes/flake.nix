1e27d3df61394059c0537c4aae91a9189b2fc11f
Daniel Moron <dansmediacorner@gmail.com>
2025-06-27 17:13:04 +0200

    Fully Migrated Config

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -2,12 +2,14 @@
   description = "Toplevel Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Keep unstable for this test
-    disko.url = "github:nix-community/disko/master";     # Keep master for this test
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    disko.url = "github:nix-community/disko/master";
     disko.inputs.nixpkgs.follows = "nixpkgs";
+	home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, disko, ... } @ inputs: {
+  outputs = { self, nixpkgs, disko, home-manager ... } @ inputs: {
     nixosConfigurations = {
       "nixos" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -15,8 +17,170 @@
           ./configuration.nix
           ./disks.nix
           disko.nixosModules.disko
+		  home-manager.nixosModules.home-manager {
+          home-manager.users.daniel = import ./homemanager/home.nix;
         ];
       };
     };
   };
+
+#Mount Harddrives
+##Create Mountpoints
+  systemd.tmpfiles.rules = [
+    "d /mnt/4TBintSSD 0755 daniel users -"
+    "d /mnt/4TBextHDD 0755 daniel users -"
+  ];
+##Mount Drives
+  fileSystems."/mnt/4TBintSSD" = {
+    device = "/dev/disk/by-uuid/c9fb51d5-39db-4ecf-a688-4c949c5c644b"; # Replace with your actual UUID
+    fsType = "btrfs";
+    options = [ "defaults" ];
+  };
+  fileSystems."/mnt/4TBextHDD" = {
+    device = "/dev/disk/by-uuid/e5e5d4f1-1a68-4d0c-819f160cdd37005c"; # Replace with your actual UUID
+    fsType = "btrfs";
+    options = [ "defaults" "compress=zstd" ];
+  };
+
+#Extra boot options
+ boot = {
+    kernelModules = [ "v4l2loopback" ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    # Appimage Support
+    binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+    };
+    plymouth.enable = true;
+  };
+ 
+#Install System Packages
+   environment.systemPackages = with pkgs; [
+##Hyprland Related
+   hypridle
+   swww
+   cliphist
+   brightnessctl
+   swaynotificationcenter
+   kdePackages.dolphin
+   kdePackages.qtsvg #icons if missing
+   kdePackages.kio-fuse #for mounting remote locations
+   kdePackages.kio-extras #more protocols for kio-fuse
+   ffmpegthumbnailer
+##Fonts
+   noto-fonts-emoji
+   noto-fonts-cjk-sans
+   font-awesome
+   symbola
+   material-icons
+   fira-code
+   fira-code-symbols
+##Basic Packages
+   wget
+   blueman
+   pavucontrol
+   wireplumber
+   appimage-run
+   lm_sensors
+   unzip
+   kdePackages.kwallet
+##UserLevel Packages
+   vesktop
+   easyeffects
+   #wineWowPackages.stable
+   mangohud
+   obsidian
+   git
+   gitkraken
+   gedit
+   gparted
+   btrfs-progs
+   unzip
+   nextcloud-client
+ ];
+  
+#Enable System Packages & Configure them
+##Hyprland
+   programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+     };
+   xdg.portal.enable = true;
+   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-hyprland];
+   xdg.portal.configPackages = [pkgs.hyprland];
+   environment.sessionVariables = {NIXOS_OZONE_WL = "1"; };
+##Flatpak Support
+   services.flatpak.enable = true;
+   systemd.services.flatpak-repo = {
+    wantedBy = ["multi-user.target"];
+    path = [pkgs.flatpak];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+   };
+##Greetd
+  services.greetd = {
+    enable = true;
+    vt = 3;
+    settings = {
+      default_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland"; # Command to execute Hyprland
+        user = "daniel"; # Replace with your actual username
+      };
+    };
+  };
+##Thunar
+   programs = {
+    xfconf.enable = true;
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-volman
+      ];
+    };
+   };
+   services.gvfs.enable = true; # Mount, trash, and other functionalities
+   services.tumbler.enable = true; # Thumbnail support for images
+##Steam
+  steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+##GamingBonusPackages  
+  gamemode.enable = true;
+  coolercontrol.enable = true;
+  coolercontrol.nvidiaSupport = true;
+  };
+
+#Nvidia Graphics Drivers
+ services.xserver.videoDrivers = ["nvidia"];
+ hardware.nvidia.modesetting.enable = true;
+ hardware.nvidia.open = true;
+ hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+ hardware.nvidia.nvidiaSettings = true;
+ hardware.graphics.enable = true;
+
+ environment.sessionVariables = {
+   __GL_SHADER_DISK_CACHE = "1";
+   __GL_SHADER_DISK_CACHE_PATH = "/mnt/4TBintSSD/NvidiaShaderCache";
+   __GL_SHADER_DISK_CACHE_SIZE = "40073741824";
+ };
+ 
+#Needed Hardware Settings
+ hardware.bluetooth.enable = true;
+ hardware.bluetooth.powerOnBoot = true;
+ services.pipewire = {
+  enable = true;
+  pulse.enable = true; };
+ services.fstrim.enable = true;
+ boot.kernelModules = [ "coretemp" "nct6775" ]; #needed for lm-sensors  
+
 }
