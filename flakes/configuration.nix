eb612f44022f2d93e805e6ae56db904525984f33
Trace <tracekotae@gmail.com>
2025-06-28 00:42:51 +0200

    fully enable stylix

--- flakes/configuration.nix
+++ flakes/configuration.nix
@@ -33,31 +33,31 @@
    
 ##THEMING   
 stylix.enable = true;
-#stylix.targets.gtk.enable = true;
-#stylix.targets.qt.enable = true;
+stylix.targets.gtk.enable = true;
+stylix.targets.qt.enable = true;
 stylix.autoEnable = true;
-#stylix.cursor.package = pkgs.bibata-cursors;
-#stylix.cursor.name = "Bibata-Modern-Ice";
-#stylix.cursor.size = 24;
-#stylix.polarity = "dark";
+stylix.cursor.package = pkgs.bibata-cursors;
+stylix.cursor.name = "Bibata-Modern-Ice";
+stylix.cursor.size = 24;
+stylix.polarity = "dark";
 stylix.image = ./Assets/Wallpapers/SakuratreeLarge.png;
-#stylix.opacity.terminal = 0.5;
-#stylix.opacity.applications = 0.5;
-#stylix.opacity.desktop = 0.5;
-#stylix.opacity.popups = 0.5;
-#stylix.fonts = {
-#	monospace = {
-#		package = pkgs.nerd-fonts.jetbrains-mono;
-#		name = "JetBrainsMono Nerd Font Mono";
-#		};
-#		
-#	sansSerif = {
-#		package = pkgs.dejavu_fonts;
-#		name = "DejaVu Sans";
-#		};
-#	serif = {
-#		package = pkgs.dejavu_fonts;
-#		name = "DejaVu Serif";
-#		};
-#	};
+stylix.opacity.terminal = 0.5;
+stylix.opacity.applications = 0.5;
+stylix.opacity.desktop = 0.5;
+stylix.opacity.popups = 0.5;
+stylix.fonts = {
+	monospace = {
+		package = pkgs.nerd-fonts.jetbrains-mono;
+		name = "JetBrainsMono Nerd Font Mono";
+		};
+		
+	sansSerif = {
+		package = pkgs.dejavu_fonts;
+		name = "DejaVu Sans";
+		};
+	serif = {
+		package = pkgs.dejavu_fonts;
+		name = "DejaVu Serif";
+		};
+	};
 }
