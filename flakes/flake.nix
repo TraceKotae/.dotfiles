055114cf6c8e3ad0edadb9ab9e13fa849cf73dc8
Trace <tracekotae@gmail.com>
2025-06-27 08:53:35 +0200

    Run Disko from flake

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -3,21 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    # Add disko as an input
     disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs"; # Ensure disko uses the same nixpkgs version
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, disko, ... } @ inputs: { # Add disko to outputs
+  outputs = { self, nixpkgs, disko, ... } @ inputs: {
     nixosConfigurations = {
       "nixos" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
           ./disks.nix
-          disko.nixosModules.disko # Import the disko NixOS module
+          disko.nixosModules.disko
         ];
       };
+    };
+
+    # Expose the disko configuration directly for use with 'nix run'
+    diskoConfig = disko.lib.diskoConfig {
+      modules = [
+        ./disks.nix
+      ];
     };
   };
 }
