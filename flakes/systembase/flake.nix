4b67d1d107d4a734bdf2269f5cc5a2f3d5306f87
Trace <tracekotae@gmail.com>
2025-06-27 20:37:23 +0200

    fix syntax

--- flakes/systembase/flake.nix
+++ flakes/systembase/flake.nix
@@ -24,32 +24,9 @@ hardware.bluetooth.enable = true;
 hardware.bluetooth.powerOnBoot = true;
 services.pipewire = {
  enable = true;
- pulse.enable = true; };
-#services.gnome.gnome-keyring.enable = true;
+ pulse.enable = true};
 services.fstrim.enable = true;
 boot.kernelModules = [ "coretemp" "nct6775" ]; #needed for lm-sensors  
-
-# Mount Harddrives
-# create dirs and assign ownership
-
-  systemd.tmpfiles.rules = [
-    "d /mnt/4TBintSSD 0755 daniel users -"
-    "d /mnt/4TBextHDD 0755 daniel users -"
-  ];
-  
-   # ... your fileSystems mounts for the Btrfs drives (from previous instructions) ...
-  fileSystems."/mnt/4TBintSSD" = {
-    device = "/dev/disk/by-uuid/c9fb51d5-39db-4ecf-a688-4c949c5c644b"; # Replace with your actual UUID
-    fsType = "btrfs";
-    options = [ "defaults" ];
-  };
-
-  fileSystems."/mnt/4TBextHDD" = {
-    device = "/dev/disk/by-uuid/e5e5d4f1-1a68-4d0c-819f160cdd37005c"; # Replace with your actual UUID
-    fsType = "btrfs";
-    options = [ "defaults" "compress=zstd" ];
-  };
 };
 };
 }
-
