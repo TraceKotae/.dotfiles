7a606946cac2343470ead1e640a9f1e19528fb1d
Trace <tracekotae@gmail.com>
2025-06-27 11:06:55 +0200

    fix stupid

--- flakes/disks.nix
+++ flakes/disks.nix
@@ -60,37 +60,3 @@
     };
   };
 }
-Step 3: Update your flake.nix
-This step brings back the import of disks.nix into your system configuration, but crucially, it still does not define the diskoConfig output.
-
-Nix
-
-{
-  description = "Toplevel Flake";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Keep unstable for this test
-    disko.url = "github:nix-community/disko/master";     # Keep master for this test
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { self, nixpkgs, disko, ... } @ inputs: {
-    nixosConfigurations = {
-      "nixos" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./disks.nix # ADD THIS LINE BACK IN
-          disko.nixosModules.disko
-        ];
-      };
-    };
-
-    # THIS ENTIRE diskoConfig BLOCK SHOULD STILL BE REMOVED:
-    # diskoConfig = disko.lib.diskoConfig {
-    #   modules = [
-    #     ./disks.nix
-    #   ];
-    # };
-  };
-}
