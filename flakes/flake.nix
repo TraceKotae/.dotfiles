24b878ce7abb7a6ae56ea26624129bba54839b8d
Trace <tracekotae@gmail.com>
2025-06-27 08:44:25 +0200

    Properly import the nix module

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -1,19 +1,23 @@
 {
-description = "Toplevel Flake";
+  description = "Toplevel Flake";
 
-inputs = {
-nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-};
-
-outputs = { self, nixpkgs, ... } @ inputs: {
- nixosConfigurations = {
-  "nixos" = nixpkgs.lib.nixosSystem {
-   system = "x86_64-linux";
-   modules = [
-    ./configuration.nix
-    ./disks.nix
-    ];
-   };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # Add disko as an input
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs"; # Ensure disko uses the same nixpkgs version
   };
- };
+
+  outputs = { self, nixpkgs, disko, ... } @ inputs: { # Add disko to outputs
+    nixosConfigurations = {
+      "nixos" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./disks.nix
+          disko.nixosModules.disko # Import the disko NixOS module
+        ];
+      };
+    };
+  };
 }
