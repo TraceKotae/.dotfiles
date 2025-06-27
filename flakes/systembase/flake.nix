81e10ac38019628f101774fb7b0ce994344b6e72
Trace <tracekotae@gmail.com>
2025-06-27 20:41:50 +0200

    test2

--- flakes/systembase/flake.nix
+++ flakes/systembase/flake.nix
@@ -2,7 +2,7 @@
 description = "Very basic system packages";
 
 inputs = {
- nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+ nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 };
 outputs = { self, nixpkgs, ... }: {
  nixosModules.default = { config, pkgs, ... }: {
