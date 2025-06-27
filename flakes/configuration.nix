755fc84860a8bbb56d17d915d3591bcb2906e849
Trace <tracekotae@gmail.com>
2025-06-27 11:58:47 +0200

    test

--- flakes/configuration.nix
+++ flakes/configuration.nix
@@ -14,8 +14,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.grub.useOSProber = true;
+ # boot.loader.efi.efiSysMountPoint = "/boot";
+ # boot.loader.grub.useOSProber = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.enableCryptodisk = true;
 
