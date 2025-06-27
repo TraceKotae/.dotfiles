4851f77000a27f988a25b8acf4eb364f802215ef
Trace <tracekotae@gmail.com>
2025-06-28 01:02:19 +0200

    Fix path to bg image

--- flakes/desktopbase/stylix.nix
+++ flakes/desktopbase/stylix.nix
@@ -5,7 +5,7 @@
   stylix.enable = true;
   stylix.autoEnable = true; # Automatically enables most of the below targets
   stylix.polarity = "dark";
-  stylix.image = ./Assets/Wallpapers/SakuratreeLarge.png;
+  stylix.image = ~/.dotfiles/flakes/Assets/Wallpapers/SakuratreeLarge.png;
 
   # Set opacities
   stylix.opacity = {
