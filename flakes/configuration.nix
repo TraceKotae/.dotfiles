245d6f8c1f14fe85a2d6ff9bf0988e83dff09901
Trace <tracekotae@gmail.com>
2025-06-27 11:41:40 +0200

    remove hw-configuration.nix

--- flakes/configuration.nix
+++ flakes/configuration.nix
@@ -6,7 +6,7 @@
 
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+ # not needed with disko     ./hardware-configuration.nix
     ];
 
 #Bootloader.
