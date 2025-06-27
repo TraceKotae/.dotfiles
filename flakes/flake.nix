2f714a95813c8e9a79a9a9c0f8b6bce5fee1f9fc
Daniel Moron <dansmediacorner@gmail.com>
2025-06-27 22:14:28 +0200

    Add Homemanager

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Keep unstable for this test
     disko.url = "github:nix-community/disko/master";     # Keep master for this test
+	home-manager.url = "github:nix-community/home-manager/unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 	nvidia.url = "path:./nvidia";
 	systembase.url = "path:./systembase";
   };
 
-  outputs = { self, nixpkgs, disko, nvidia, systembase, ... } @ inputs: {
+  outputs = { self, nixpkgs, disko, nvidia, systembase, home-manager, ... } @ inputs: {
     nixosConfigurations = {
       "nixos" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -19,6 +21,9 @@
           disko.nixosModules.disko
 		  nvidia.nixosModules.default
 		  systembase.nixosModules.default
+		  home-manager.nixosModules.home-manager {
+          home-manager.users.daniel = import ./home.nix;
+          }
         ];
       };
     };
