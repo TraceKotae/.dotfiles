e2c12cbf9f2ed358d86d035033b383549f171346
Trace <tracekotae@gmail.com>
2025-06-27 20:13:44 +0200

    Update Nvidia Flake to track unstable

--- flakes/nvidia/flake.nix
+++ flakes/nvidia/flake.nix
@@ -2,7 +2,7 @@
 description = "Wayland Base Desktop Configuration";
 
 inputs = {
- nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+ nixpkgs.url = "github:NixOS/nixpkgs/unstable";
 };
 outputs = { self, nixpkgs, ... }: {
  nixosModules.default = { config, pkgs, ... }: {
