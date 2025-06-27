ab9b22c26a5c605b67e1ef487277fe7c41e74ed4
Trace <tracekotae@gmail.com>
2025-06-28 01:03:58 +0200

    same

--- flakes/desktopbase/stylix.nix
+++ flakes/desktopbase/stylix.nix
@@ -5,7 +5,7 @@
   stylix.enable = true;
   stylix.autoEnable = true; # Automatically enables most of the below targets
   stylix.polarity = "dark";
-  stylix.image = home/daniel/.dotfiles/flakes/Assets/Wallpapers/SakuratreeLarge.png;
+  stylix.image = ./SakuratreeLarge.png;
 
   # Set opacities
   stylix.opacity = {
