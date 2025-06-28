7d1a48af5d8caa79ca5db81539f4fcef46cc7dbf
Trace <tracekotae@gmail.com>
2025-06-28 02:28:03 +0200

    update start script

--- flakes/Assets/Hyprland/initialstart.sh
+++ flakes/Assets/Hyprland/initialstart.sh
@@ -1,2 +1,5 @@
 swww-daemon &
 swww img ~/.dotfiles/flakes/desktopbase/Wallpapers/SakuratreeLarge.png &
+sleep 2 &
+qdbus org.kde.kwalletd5 /modules/kwalletd5 org.kde.KWallet.open kdewallet 0 "Hyprland" &
+
