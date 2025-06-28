03e488ca8458a4c99fa248ae354cd2cff3e6ad7f
Trace <tracekotae@gmail.com>
2025-06-28 02:32:57 +0200

    add additional unlock option

--- flakes/Assets/Hyprland/initialstart.sh
+++ flakes/Assets/Hyprland/initialstart.sh
@@ -2,4 +2,5 @@ swww-daemon &
 swww img ~/.dotfiles/flakes/desktopbase/Wallpapers/SakuratreeLarge.png &
 sleep 2 &
 qdbus org.kde.kwalletd5 /modules/kwalletd5 org.kde.KWallet.open kdewallet 0 "Hyprland" &
+kwallet-query -l kdewallet &
 
