039e1398d189a02edba20ae3949a7b14b0a078c0
Daniel Moron <dansmediacorner@gmail.com>
2025-06-28 07:50:53 +0000

    Make executable

--- flakes/Assets/Hyprland/initialstart.sh
+++ flakes/Assets/Hyprland/initialstart.sh
@@ -1,6 +1,7 @@
 swww-daemon &
 swww img ~/.dotfiles/flakes/desktopbase/Wallpapers/SakuratreeLarge.png &
-sleep 2 &
+sleep 2 &&
 qdbus org.kde.kwalletd5 /modules/kwalletd5 org.kde.KWallet.open kdewallet 0 "Hyprland" &
 kwallet-query -l kdewallet &
-
+sleep 10 &&
+nextcloud &
